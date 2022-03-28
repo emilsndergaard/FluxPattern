63204e7e0c54c2561a5fb844111ebaeb902f7684
Sndergaard <emilsndergaard>
2022-03-28 14:12:47 +0200

    :sparkles:Initial Commit

---
+++ FluxPattern_WPF/FluxPattern_WPF/Store/IStore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluxPattern_WPF.Store
+{
+    internal class IStore
+    {
+    }
+}
